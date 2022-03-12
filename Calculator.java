699317011180d6f1822c7d5c28e14fa2eba26c4a
Cassa <firasgara@hotmail.fr>
2022-03-12 23:11:21 +0100

    new branch

--- Calculator.java
+++ Calculator.java
@@ -16,7 +16,7 @@ public class Calculator extends javax.swing.JFrame {
     /**
      * Creates new form Calculator
      */
-//git modification
+//git modification another modification
     public Calculator() {
         initComponents();
     }
